d29f54f2c476112f2f6c405416f7f8d616080388
Anass Zaki <thezaki.anass@gmail.com>
2019-02-06 23:21:50 +0100

    accessing JSONs

---
+++ JavaScript/accessingAnObject.js
@@ -0,0 +1,32 @@
+var collection = {
+    "100": {
+        "album": "Lateralus",
+        "artist": "Tool",
+        "tracks": [
+            "The Grudge",
+            "The Patient"
+        ]
+    },
+    "1000": {
+        "album": "Aenima",
+        "artist": "Tool",
+        "tracks": [
+            "aenima",
+            "Useful Idiot"
+        ]
+    },
+    "10000": {
+        "album": "10,000 Days",
+        "artist": "Robert Palmer",
+        "tracks": [
+            "The Pot",
+            "Jambi",
+            "Vicarious"
+        ]
+    },
+};
+
+var secondTree = myPlants[0].list[2];
+
+console.log(secondTree);
+
