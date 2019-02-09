21e754cf21ceda176d017105b6462946a3bd5b4a
Anass Zaki <thezaki.anass@gmail.com>
2019-02-09 19:24:59 +0100

    checks if a string is a palindrome

---
+++ JavaScript/reverseString.js
@@ -0,0 +1,17 @@
+function reverseString(str) {
+    // Create an empty string that will host the new created string
+    var input = "";
+
+    // for loop that will iterate over the string characters 
+    for(var i = str.length - 1; i >= 0; i--){
+        // assigns each last character to the string
+        input += str[i];
+    }
+    
+    if (input === reverseString) {
+        return true;
+    } else {
+        return false;
+    }
+}
+console.log(reverseString("string"));
