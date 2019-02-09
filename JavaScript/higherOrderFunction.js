7684dec791ebda69c6b4f7d2e6aa5728761396b4
Anass Zaki <thezaki.anass@gmail.com>
2019-02-09 19:22:01 +0100

    made this code more readable

--- JavaScript/higherOrderFunction.js
+++ JavaScript/higherOrderFunction.js
@@ -4,9 +4,12 @@ const arrayOfRealNumbers = [1.45, 5, 2574, 3, -96, 6, 7, -12];
 // creates a function that has multiple functions within it
 const squareList = (arr) => {
   "use strict";
-  const squaredIntegers = arr.filter((number) => number > 0 && number % parseInt(number) === 0).map((number) => Math.pow(number, 2))
+  const squaredIntegers = arr
+    .filter((number) => number > 0 && number % parseInt(number) === 0)
+    .map((number) => Math.pow(number, 2))
+    
   return squaredIntegers; 
 };
 // prints the squared integers
 const squaredIntegers = squareList(arrayOfRealNumbers);
-console.log(squaredIntegers);
+console.log(squaredIntegers); 
