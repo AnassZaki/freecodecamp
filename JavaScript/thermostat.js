a13b1840de2809515c143fe1ac84c88bed8df823
Anass Zaki <thezaki.anass@gmail.com>
2019-02-11 20:30:01 +0100

    using class syntax to make a thermostat function

---
+++ JavaScript/thermostat.js
@@ -0,0 +1,22 @@
+function makeClass() {
+    "use strict";
+    class Thermostat {
+        constructor(fahrenheit) {
+            this.fahrenheit = fahrenheit;
+        }
+        //getter 
+        get temperature() {
+            return 5 / 9 * (this.fahrenheit - 32);
+        }
+        //setter
+        set temperature(celsius) {
+            this.fahrenheit = celsius * 9.0 / 5 + 32;
+        }
+    }
+    return Thermostat;
+}
+const Thermostat = makeClass();
+const thermos = new Thermostat(76); // setting in Fahrenheit scale
+let temp = thermos.temperature; // 24.44 in C
+thermos.temperature = 26;
+temp = thermos.temperature; // 26 in C
