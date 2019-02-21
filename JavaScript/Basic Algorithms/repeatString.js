494cc00508efbb972a65895d1d7029d8998ac858
Anass Zaki <thezaki.anass@gmail.com>
2019-02-21 20:33:29 +0100

    repeating a string n times using repeat() method

---
+++ JavaScript/Basic Algorithms/repeatString.js
@@ -0,0 +1,10 @@
+// very complicated code
+// repeat(number) method
+function repeatStringNumTimes(str, num) {
+    
+    let newStr = str.repeat(num);
+
+    return newStr;
+}
+
+console.log(repeatStringNumTimes("abc", 3));
