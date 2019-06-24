89e92cdf90a6b5486bf3f6423d3f10dfd3005b29
Anass Zaki <thezaki.anass@gmail.com>
2019-06-24 20:57:53 +0200

    Revert "Intermediate Algorithm Scripting Challenges Solved"
    This reverts commit 053d119c7a03881b26e256839f5889c09a55ba79.

--- JavaScript/Intermediate Algorithms/pigLatin.js
+++
@@ -1,9 +0,0 @@
-function makePigLatin(str) {
-    let arr = [];
-    for (let i = 0; i < str.length; i++) {
-        arr.push(str[i]);
-        arr[0] + 'ay'
-    }
-    return arr;
-}
-console.log(makePigLatin("batata"));
--- JavaScript/Intermediate Algorithms/seekAndDestroy.js
+++
@@ -1,5 +0,0 @@
-function seekAndDestroy(arr) {
-    let args = [].slice.call(arguments);
-    return arr.filter((x) => !args.includes(x));
-}
-console.log(seekAndDestroy([1, 2, 3, 1, 2], 1, 2));
