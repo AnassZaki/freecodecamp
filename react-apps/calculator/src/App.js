48412c02893793e0cd120133cdcd721f2c360f5e
Anass Zaki <thezaki.anass@gmail.com>
2019-06-28 23:50:51 +0200

    second commit for the calculator app

---
+++ react-apps/calculator/src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './App.css';
+import Calculator from './Components/Calculator';
+
+function App() {
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <div className="container">
+          <Calculator />
+        </div>
+      </header>
+    </div>
+  );
+}
+
+export default App;
